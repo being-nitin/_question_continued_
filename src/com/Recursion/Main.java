be59b6c92830de46a1507f7dd77003d104420c3d
Nitin <nsinghboss02@gmail.com>
2022-01-25 02:43:05 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,29 @@
+package com.Recursion;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	 Given an input string S and two characters char1 and char2, you need to replace every occurrence of character c1 with character c2 in the given string. Do this recursively.
+
+     Sample Input :
+     abaca
+     a x
+     Sample Output :
+     xbxcd
+	 */
+
+
+
+
+    }
+    public static String replace(String str,char ch1,char ch2){
+        str.toCharArray();
+        for(int i=0;i<str.length();i++){
+            if(str[i]==ch1){
+
+            }
+        }
+
+    }
+}
